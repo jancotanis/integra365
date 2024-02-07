45e23352073759c0de8d514a93d7e56e4cf502b3
Janco Tanis <git@jancology.com>
2024-02-07 08:44:47 +0100

    use const

--- lib/integra365.rb
+++ lib/integra365.rb
@@ -7,13 +7,22 @@ module Integra365
   extend WrAPI::Configuration
   extend WrAPI::RespondTo
 
+  DEFAULT_ENDPOINT = 'https://api.integra-bcs.nl/Api/V1/'.freeze
+  DEFAULT_UA       = "Integra365 Ruby API wrapper #{Integra365::VERSION}".freeze
+
   # Alias for Integra365::Client.new
   #
   # @return [Integra365::Client]
   def self.client(options = {})
     Integra365::Client.new({
-      endpoint: 'https://api.integra-bcs.nl/Api/V1/'.freeze,
-      user_agent: "Integra365 Ruby API wrapper #{Integra365::VERSION}".freeze
+      endpoint: DEFAULT_ENDPOINT,
+      user_agent: DEFAULT_UA
     }.merge(options))
+  end
+
+  def self.reset
+    super
+    self.endpoint = DEFAULT_ENDPOINT
+    self.user_agent = DEFAULT_UA
   end
 end
